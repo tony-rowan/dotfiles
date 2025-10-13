054d46f7a47f8c525cb0306411e9d74279bbe278
Tony Rowan <tony@char.gy>
2025-10-13 12:25:05 +0100

    Remove sublime setup

--- sublime-user-package/files.py
+++
@@ -1,9 +0,0 @@
-import sublime
-import sublime_plugin
-
-import os
-
-class DeleteFileCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        os.remove(self.view.file_name())
-        self.view.close()
